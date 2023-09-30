1e806fde5e3203b5ebd0623948b128e15fa6b841
jlhillery999 <jlhillery999@gmail.com>
2023-09-30 12:47:12 -0400

    Configured Provider Block

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.84.0"
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "4.84.0"
+        }
     }
-  }
 }
 
 provider "google" {
-  # Configuration options
+    project = var.PROJECT_ID
+    region = var.REGION
 }
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "PROJECT_ID" {
+    description=    "The project ID"
+    type=           string
+}
+
+variable "PROJECT_NUMBER" {
+    description=    "The project's number"
+    type=           string
+}
+
+variable "REGION" {
+    description=    "The default region for the project's resources"
+    type=           string
+}
